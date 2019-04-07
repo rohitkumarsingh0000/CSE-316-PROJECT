fa9dff7cc851e522d473b5b310738642494ba86d
rohitkumarsingh0000 <49370465+rohitkumarsingh0000@users.noreply.github.com>
2019-04-07 23:38:25 +0530

    Add files via upload

---
+++ A.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,p,a,d;
+	  printf("Enter the number of passengers:");
+      scanf("%d", &n);
+        printf("\n");
+      int arr[n];
+        printf("Enter the details  of the passengers:\n\n\n");
+	     
+	     
+	     for (i = 0; i < n; i++) {
+	
+   scanf("P%d", arr[n]);
+   printf("The arrival time of pessanger %d is:",i+1);
+   scanf("%d", &a);
+   printf("The distance to be travelled by passanger %d is:",i+1);
+   scanf("%d", &d);
+     printf("\n");
+}
+}
